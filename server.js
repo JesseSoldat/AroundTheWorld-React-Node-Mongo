ee12f7568362be1524645b54534d0a6d08c82370
JLab <jessesoldat@gmail.com>
2018-10-15 09:15:19 -0500

    in production serve static files and send index.html

--- server.js
+++ server.js
@@ -15,3 +15,18 @@ app.use(helmet());
 app.use(bodyParser.json());
 
 require("./server/routes/auth")(app);
+
+if (process.env.NODE_ENV === "production") {
+  // express will serve up production assets (main.js or main.css)
+  const path = require("path");
+  app.use(express.static(path.resolve(__dirname, "client", "build")));
+
+  //express will serve up index.html if it doesn't recognize the route
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("*", (req, res) => {
+    res.send(`A request was made to ${req.url}`);
+  });
+}
